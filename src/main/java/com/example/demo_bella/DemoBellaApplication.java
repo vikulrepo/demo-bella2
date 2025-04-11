92c2560c43478e5ac3cb01ed484e5986a40f84a5
Vikul-mentor <you@example.com>
2025-04-11 15:33:50 +0530

    second from VS code

---
+++ src/main/java/com/example/demo_bella/DemoBellaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_bella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_bella/DemoBellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_bella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
